d5c4cf7f23885ba7b5c63b65cc3a926289ed6409
PrynsTag <201812311@fit.edu.ph>
2021-05-22 11:56:59 +0800

    refactored code.

--- Python_Home_Work_Section2_Law_of_Large_1.py
+++ Python_Home_Work_Section2_Law_of_Large_1.py
@@ -1,11 +1,12 @@
 import numpy as np
 from numpy.random import randn
+
 counter = 0
-num_tries = 10 # Sample Size
+num_tries = 10  # Sample Size
 
 for i in randn(num_tries):
-    if i >= -1 and i <= 1:
-        counter += 1 # Increase counter by 1 when True
+    if -1 <= i <= 1:
+        counter += 1  # Increase counter by 1 when i falls between -1 and 1
 
-percent_ans = np.mean(counter/num_tries)*100
+percent_ans = np.mean(counter / num_tries) * 100
 print(percent_ans)
